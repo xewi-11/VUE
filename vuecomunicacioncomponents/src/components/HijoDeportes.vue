b149d198492f9a6c7d8a0e09dd6d218ff412904b
xewi-11 <javi11coimbra@gmail.com>
2025-10-27 14:15:03 +0100

    feat: initialize vuecomunicacioncomponents project with basic structure and components
    - Added package.json for project dependencies and scripts
    - Created public/index.html and public/favicon.ico for application entry
    - Implemented main App.vue component with PadreDeportes as a child component
    - Added HelloWorld component with documentation links
    - Created HijoDeportes and PadreDeportes components for sports display
    - Included logo image in assets
    - Configured main.js for Vue app initialization
    - Added vue.config.js for Vue CLI service configuration

---
+++ vuecomunicacioncomponents/src/components/HijoDeportes.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 style="color: red">Hijo Deportes Component</h1>
+
+    <ul>
+      <li v-for="dep in deportes" :key="dep">{{ dep }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HijoDeportes",
+  props: ["deportes"],
+};
+</script>
+
+<style></style>
---
+++ vuecomunicacioncomponents/src/components/PadreDeportes.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 style="color: blue">Padre Deportes Component</h1>
+    <hr />
+    <HijoDeportes :deportes="deportes" />
+  </div>
+</template>
+
+<script>
+import HijoDeportes from "./HijoDeportes.vue";
+export default {
+  name: "PadreDeportes",
+  components: {
+    HijoDeportes,
+  },
+  data() {
+    return {
+      deportes: ["Futbol", "Baloncesto", "Tenis", "Rugby"],
+    };
+  },
+};
+</script>
+
+<style></style>
