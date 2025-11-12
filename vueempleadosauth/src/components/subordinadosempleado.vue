ac736737f8be4e517c890a4c9db8624d83bb7bbe
xewi-11 <javi11coimbra@gmail.com>
2025-11-12 17:42:15 +0100

    feat: agregar componente SubordinadosEmpleado para mostrar subordinados de un empleado

---
+++ vueempleadosauth/src/components/subordinadosempleado.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <h1>Subordinados Empleado</h1>
+        <table v-if="subordinados">
+            <thead>
+                <tr>
+                    <th>Oficio</th>
+                    <th>Apellido</th>
+                    <th>Director</th>
+                    <th>Salario</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="subordinado in subordinados" :key="subordinado.id">
+                    <td>{{ subordinado.oficio }}</td>
+                    <td>{{ subordinado.apellido }}</td>
+                    <td>{{ subordinado.director }}</td>
+                    <td>{{ subordinado.salario }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import ServiceEmpleados from '@/services/serviceempleados';
+
+const service = new ServiceEmpleados();
+export default {
+    name: "SubordinadosEmpleado",
+    data() {
+        return {
+            subordinados:null
+        }
+    },
+    methods: {
+        cargarSubordinados(){
+            service.getSubordinadosPorEmpleado().then(response=>{
+                this.subordinados=response.data;
+            }).catch(error=>{
+                console.log(error);
+            });
+        }
+    },
+    mounted(){
+        this.cargarSubordinados();
+    }
+}
+</script>
+
+<style>
+
+</style>
